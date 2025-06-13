4f7382a783e0a8e9ea212056daadad3d1fdae4dd
jonlindholm11 <jonlindholm11@gmail.com>
2025-06-12 22:46:31 -0400

    prompt

---
+++ index.js
@@ -0,0 +1,6 @@
+const userInput = userInputString = prompt(
+  "Hello, welcome to our online ice cream store would you like? please enter your order using a comma between each flavor",
+  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+);
+console.log(userInput.split(","))
+
