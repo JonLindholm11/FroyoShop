6d6fdd9f1be406cc38b3c8bd1ce2f00eb8ff41cb
jonlindholm11 <jonlindholm11@gmail.com>
2025-06-12 22:48:11 -0400

    split

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ const userInput = userInputString = prompt(
   "Hello, welcome to our online ice cream store would you like? please enter your order using a comma between each flavor",
   "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
 );
-console.log(userInput.split(","))
-
+const arr = userInput.split(",");
